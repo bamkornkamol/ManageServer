d3eaa73ae1187bd2aceab73310ac3bf779aa6acd
bamkornkamol <88421920+bamkornkamol@users.noreply.github.com>
2023-11-12 14:19:32 +0700

    ManageServer

--- src/main/java/com/sop/manageserver/pojo/Member.java
+++ src/main/java/com/sop/manageserver/pojo/Member.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document(collection = "Server")
+@Document(collection = "Member")
 public class Member {
     @Id
     private String _id;
